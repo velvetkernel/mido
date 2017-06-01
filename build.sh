65edd9d0a85bf3871acf00b6687eefe385258fe7
Arnav Gosain <arn4v@outlook.com>
2017-06-01 17:29:24 +0530

    velvet

---
+++ build.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+echo "Welcome to Velvet Kernel Builder!"
+LC_ALL=C date +%Y-%m-%d
+kernel_dir=$PWD
+build=$kernel_dir/out
+export CROSS_COMPILE="/home/arn4v/velvet/toolchains/aarch64-uber-4.9/bin/aarch64-linux-android-"
+kernel="velvet"
+version="001"
+vendor="xiaomi"
+device="mido-los"
+zip=zip
+date=`date +"%Y%m%d-%H%M"`
+config=mido_defconfig
+kerneltype="Image.gz-dtb"
+jobcount="-j$(grep -c ^processor /proc/cpuinfo)"
+#modules_dir=$kernel_dir/"$zip"/system/lib/modules
+modules_dir=$kernel_dir/"$zip"/modules
+zip_name="$kernel"-"$version"-"$device".zip
+export KBUILD_BUILD_USER=arnavgosain
+export KBUILD_BUILD_HOST=velvet
+
+echo "Checking for build..."
+if [ -d arch/arm64/boot/"$kerneltype" ]; then
+	read -p "Previous build found, clean working directory..(y/n)? : " cchoice
+	case "$cchoice" in
+		y|Y )
+			rm -rf out
+			mkdir out
+			rm -rf "$zip"/modules
+			mkdir "$zip"/modules
+			export ARCH=arm64
+			make clean && make mrproper
+			echo "Working directory cleaned...";;
+		n|N )
+			exit 0;;
+		* )
+			echo "Invalid...";;
+	esac
+	read -p "Begin build now..(y/n)? : " dchoice
+	case "$dchoice" in
+		y|Y)
+			make "$config"
+			make "$jobcount"
+			exit 0;;
+
+		n|N )
+			exit 0;;
+		* )
+			echo "Invalid...";;
+	esac
+fi
+echo "Extracting files..."
+if [ -f arch/arm64/boot/"$kerneltype" ]; then
+	cp arch/arm64/boot/"$kerneltype" "$zip"/"$kerneltype"
+#        mkdir -p zip/modules/pronto
+#	cp drivers/staging/prima/wlan.ko zip/modules/pronto/pronto_wlan.ko
+	find . -name '*.ko' -exec cp {} $modules_dir/ \;
+	"$CROSS_COMPILE"strip --strip-unneeded "$zip"/modules/*.ko &> /dev/null
+        mkdir -p zip/modules/pronto/
+        mv zip/modules/wlan.ko zip/modules/pronto/pronto_wlan.ko
+else
+	echo "Nothing has been made..."
+	read -p "Clean working directory..(y/n)? : " achoice
+	case "$achoice" in
+		y|Y )
+                        rm -rf out
+                        mkdir out
+                        rm -rf "$zip"/modules
+                        mkdir "$zip"/modules
+			export ARCH=arm64
+                        make clean && make mrproper
+                        echo "Working directory cleaned...";;
+		n|N )
+			exit 0;;
+		* )
+			echo "Invalid...";;
+	esac
+	read -p "Begin build now..(y/n)? : " bchoice
+	case "$bchoice" in
+		y|Y)
+			make "$config"
+			make "$jobcount"
+			exit 0;;
+		n|N )
+			exit 0;;
+		* )
+			echo "Invalid...";;
+	esac
+fi
+
+echo "Zipping..."
+if [ -f "$zip"/"$kerneltype" ]; then
+	cd "$zip"
+	zip -r ../$zip_name .
+	mv ../$zip_name $build
+	rm "$kerneltype"
+	cd ..
+	rm -rf arch/arm64/boot/"$kerneltype"
+	export outdir="$build"
+	echo "Package complete: "$build"/"$zip_name""
+	exit 0;
+else
+	echo "No $kerneltype found..."
+	exit 0;
+fi
+# Export script by Savoca
+# Thank You Savoca!

606eb624c8b1dd793adb7991956b213e514e8d91
Arnav Gosain <arn4v@outlook.com>
2017-08-16 01:01:55 +0530

    fix build2.sh

--- build2.sh
+++ build2.sh
@@ -13,8 +13,7 @@ date=`date +"%Y%m%d-%H%M"`
 config=mido_defconfig
 kerneltype="Image.gz-dtb"
 jobcount="-j$(grep -c ^processor /proc/cpuinfo)"
-if [ $module == "magisk" ];
-then
+if [ $module = "magisk" ]; then
     modules_dir=$kernel_dir/modules/system/lib/modules
 else
     modules_dir=$kernel_dir/"$zip"/modules

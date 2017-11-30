710053a6de9b2d11de45097879ea83502076d54e
Adesh15 <adesikha15@gmail.com>
2017-11-30 07:24:51 +0100

    Add build script
    Signed-off-by: Adesh15 <adesikha15@gmail.com>

---
+++ build.sh
@@ -0,0 +1,14 @@
+CCACHE=$(command -v ccache)
+
+TOOLCHAIN=/home/adesh/Adesh/kernel/gcc-prebuilts/bin/aarch64-linaro-linux-android-
+
+export CROSS_COMPILE="${CCACHE} ${TOOLCHAIN}"
+
+export ARCH=arm64
+
+make clean O=out/
+make mrproper O=out/
+
+make mido_defconfig O=out/
+
+make -j8 O=out/

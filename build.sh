271d5eaf35969b6a47e400d0fcd70482797f6b93
Arnav Gosain <arn4v@outlook.com>
2018-09-10 02:21:29 +0530

    zip: fix

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Welcome to Velvet Kernel Builder!"
 LC_ALL=C date +%Y-%m-%d
-kernel_dir=$PWD/
-build=$kernel_dir/out
+kernel_dir=$PWD
+build=$kernel_dir/out2
 version=r7
 kernel="velvet"
 vendor="xiaomi"
-device="mido"
+device="mido-oreo"
 export CROSS_COMPILE="/home/arn4v/velvet/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
 zip=zip
 date=`date +"%Y%m%d-%H%M"`
@@ -20,11 +20,11 @@ export KBUILD_BUILD_USER=arnavgosain
 export KBUILD_BUILD_HOST=velvet
 
 function clean() {
-rm -rf out; mkdir out; export ARCH=arm64; make clean && make mrproper
+rm -rf out2; mkdir out2; export ARCH=arm64; make O=out clean && make O=out mrproper
 }
 
 function build() {
-make "$config"; make "$jobcount"; cp arch/arm64/boot/"$kerneltype" "$zip"/"$kerneltype"
+make O=out "$config"; make O=out "$jobcount"; cp out/arch/arm64/boot/"$kerneltype" "$zip"/"$kerneltype"
 }
 
 function cleanbuild() {
@@ -32,24 +32,18 @@ clean; build; kzip
 }
 
 function kzip() {
-rm ${HOME}/velvet/builderbot/velvet.txt
 cd $kernel_dir/$zip
 mkdir treble
 mkdir nontreble
-cp $kernel_dir/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-mido-nontreble.dtb $REPACK_DIR/nontreble/
-cp $kernel_dir/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-mido-treble.dtb $REPACK_DIR/treble/
+cp $kernel_dir/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-mido-nontreble.dtb $kernel_dir/zip/nontreble/
+cp $kernel_dir/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-mido-treble.dtb $kernel_dir/zip/treble/
 zip -r $build/$zip_name .
 rm "$kerneltype"
 cd ..
-echo "Generating changelog..."
-git --no-pager log --pretty=oneline --abbrev-commit 299f8a88d11ab7dc5c5d11a3f2ac7c1f65687868..HEAD > zip/changelog.txt
-paste $kernel_dir/$zip/changelog.txt
-rm $kernel_dir/$zip/changelog.txt
-export outdir=""$build""
-export out=""$build""
-export OUT=""$build""
+export out2dir=""$build""
+export out2=""$build""
+export out2=""$build""
 echo "Package complete: "$build"/"$zip_name""
-echo "$build/$zip_name" >> ${HOME}/velvet/builderbot/velvet.txt
 exit 0;
 }
 
@@ -65,7 +59,7 @@ else
         git add -A
         git commit -am "lol"
         git fetch origin
-        git checkout origin/cm-14.1
+        git checkout2 origin/cm-14.1
         git reset --hard
         clean
         build
@@ -78,7 +72,7 @@ then
     git add -A
     git commit -am "lol"
     git fetch origin
-    git checkout origin/cm-14.1
+    git checkout2 origin/cm-14.1
     git reset --hard
 fi
 
